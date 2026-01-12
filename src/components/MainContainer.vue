ad56d8b687067b4db803693b1c159a237c1d1f29
hjy080530 <bomaye119@gmail.com>
2026-01-12 23:08:50 +0900

    feat: 메인컴포넌트 재구성

---
+++ src/components/MainContainer.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  padding: 1rem;
+  background-color: #fff;
+}
+</style>
